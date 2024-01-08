0f9df34a4e0c74ae7f51404bfcbe05d36a6f22d3
CoderSid063 <sudhansupanda063@gmail.com>
2024-01-08 15:54:38 +0530

    feat:Integrated API to fetch movie data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     apiTest();
   }, []);
@@ -9,9 +15,15 @@ function App() {
   const apiTest = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
